5145401f7d6b7e08ae7f2ab63fd69f5e248c7eed
shangesh-tech <shangesh2006@gmail.com>
2025-01-16 17:13:39 +0530

    Update

--- index.js
+++ index.js
@@ -1,15 +1,19 @@
+// Import required modules
 const express = require('express');
-const { resolve } = require('path');
+require('dotenv').config();
 
 const app = express();
-const port = 3010;
 
-app.use(express.static('static'));
-
+// Route to return data based on IS_ADMIN value
 app.get('/', (req, res) => {
-  res.sendFile(resolve(__dirname, 'pages/index.html'));
+  if (process.env.IS_ADMIN === 'true') {
+    res.send({ message: "Welcome, Admin!", data: ["Admin Data 1", "Admin Data 2"] });
+  } else {
+    res.send({ message: "Welcome, User!", data: ["User Data 1", "User Data 2"] });
+  }
 });
 
-app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`);
+// Start the server
+app.listen(3000, () => {
+  console.log("Server is running on http://localhost:3000");
 });
